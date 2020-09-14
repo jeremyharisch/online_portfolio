e8c4e5ddf1b1d774fe9240599a1cca1e30838cb7
Jeremy Harisch <jeremy@harisch.de>
2020-09-14 12:28:26 +0200

    New-Start

---
+++ my-portfolio/src/components/introduction.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import data from '../mydata';
+
+export default class Introduction extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
